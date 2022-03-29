0d596a1d47fb8ed43644eb50197531e7c8a106c4
siyual-park <siyual.bak@gmail.com>
2022-03-29 16:10:14 +0900

    feat: add codeql

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,7 +4,7 @@ module.exports = {
   ],
   addons: [
     '@storybook/addon-links',
-    '@storybook/addon-essentials'
+    '@storybook/addon-essentials',
   ],
   features: {
     babelModeV7: true,
