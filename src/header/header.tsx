c9df21460b9632caef7ddb242d9978edd8194931
siyual-park <siyual.bak@gmail.com>
2022-03-31 21:22:19 +0900

    feat: package 수정

--- src/header/header.tsx
+++ src/header/header.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import { css } from '@linaria/core';
-import { modularScale, hiDPI } from 'polished';
 
 // Write your styles in `css` tag
 const header = css`
   text-transform: uppercase;
-  font-size: ${modularScale(2)};
   background-color: red;
-
-  ${hiDPI(1.5)} {
-    font-size: ${modularScale(2.5)};
-  }
 `;
 
 const Header: React.FC = () => <h1 className={header}>Hello world</h1>;
