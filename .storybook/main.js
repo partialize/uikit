308711d54f6098183e135b74bfafb772e32faea8
siyual-park <siyual.bak@gmail.com>
2022-03-31 19:40:01 +0900

    fix: storybook 빌드 오류 수정

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
-// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   stories: [
@@ -12,44 +12,41 @@ module.exports = {
   features: {
     babelModeV7: true,
   },
-  // webpackFinal: async (config) => {
-  //   // config.plugins.push(new MiniCssExtractPlugin({ filename: 'styles.css' }));
-  //
-  //   // config.module.rules[7] = {
-  //   //   test: /\.css$/,
-  //   //   sideEffects: true,
-  //   //   use: [
-  //   //     {
-  //   //       loader: MiniCssExtractPlugin.loader,
-  //   //       options: {
-  //   //         sourceMap: process.env.NODE_ENV !== 'production',
-  //   //       },
-  //   //     },
-  //   //     {
-  //   //       loader: 'css-loader',
-  //   //       options: { sourceMap: true },
-  //   //     },
-  //   //   ],
-  //   // };
-  //   //
-  //   // config.module.rules.push(
-  //   //   {
-  //   //     test: /\.(ts|tsx)$/,
-  //   //     exclude: /node_modules/,
-  //   //     use: [
-  //   //       { loader: 'babel-loader' },
-  //   //       {
-  //   //         loader: '@linaria/webpack-loader',
-  //   //         options: { sourceMap: true },
-  //   //       },
-  //   //     ],
-  //   //   },
-  //   //   {
-  //   //     test: /\.(jpg|png|gif|woff|woff2|eot|ttf|svg)$/,
-  //   //     use: [{ loader: 'file-loader' }],
-  //   //   },
-  //   // );
-  //
-  //   return config;
-  // },
+  webpackFinal: async (config) => {
+    config.plugins.push(new MiniCssExtractPlugin({ filename: 'styles.css' }));
+
+    config.module.rules[7] = {
+      test: /\.css$/,
+      sideEffects: true,
+      use: [
+        {
+          loader: MiniCssExtractPlugin.loader
+        },
+        {
+          loader: 'css-loader',
+          options: { sourceMap: true },
+        },
+      ],
+    };
+
+    config.module.rules.push(
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'babel-loader' },
+          {
+            loader: '@linaria/webpack-loader',
+            options: { sourceMap: true },
+          },
+        ],
+      },
+      {
+        test: /\.(jpg|png|gif|woff|woff2|eot|ttf|svg)$/,
+        use: [{ loader: 'file-loader' }],
+      },
+    );
+
+    return config;
+  },
 }
