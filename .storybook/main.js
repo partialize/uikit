b68c959ac646cb45587e6fa22131a32b7cb3adc7
siyual-park <siyual.bak@gmail.com>
2022-03-31 20:34:24 +0900

    feat: mini-css-extract-plugin 추가

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
@@ -5,12 +7,27 @@ module.exports = {
     builder: 'webpack5'
   },
   webpackFinal: (config) => {
+    config.plugins.push(new MiniCssExtractPlugin({ filename: 'styles.css' }));
+
     config.module.rules[0].use.push({
       loader: '@linaria/webpack-loader',
       options: {
         sourceMap: true
       }
     });
+    config.module.rules[7] = {
+      test: /\.css$/,
+      use: [
+        {
+          loader: MiniCssExtractPlugin.loader,
+        },
+        {
+          loader: 'css-loader',
+          options: { sourceMap: true },
+        },
+      ],
+    };
+
     return config;
   }
 };
