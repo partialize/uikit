4748bf78348c7c8efc198ff1a44c166cf20e2b30
siyual-park <siyual.bak@gmail.com>
2022-04-07 19:51:39 +0900

    feat: add index.ts

---
+++ src/core/AutoLayout/index.ts
@@ -0,0 +1,5 @@
+import AutoLayout from './AutoLayout';
+
+export type { default as AutoLayoutProps } from './AutoLayoutProps';
+export type { default as AutoLayoutTypeMap } from './AutoLayoutTypeMap';
+export default AutoLayout;
